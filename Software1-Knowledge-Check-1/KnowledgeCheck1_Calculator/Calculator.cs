ebc70d75e0a6e0bb69ece7c8b6c07bfe293e23e5
Matthew London <36553559+matthewlondon@users.noreply.github.com>
2023-02-18 13:41:08 -0500

    Add files via upload

---
+++ Software1-Knowledge-Check-1/KnowledgeCheck1_Calculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace KnowledgeCheck1_Calculator
+{
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        public int Subtract(int first, int second)
+        {
+            return first - second;
+        }
+
+        public int Multiply(int first, int second)
+        {
+            return first * second;
+        }
+
+        public double Divide(double first, double second)
+        {
+            return first / second;
+        }
+    }
+}
